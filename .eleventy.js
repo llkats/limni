8921b723491a5c4fc4b5071a663ddfd287787557
Lydia Kats <llkats@gmail.com>
2023-01-02 15:13:07 -0600

    layout aliases neat

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addLayoutAlias('post', 'post.njk')
+  eleventyConfig.addLayoutAlias('blog', 'blog.njk')
+
   eleventyConfig.addPassthroughCopy("./src/index.css")
   eleventyConfig.addPassthroughCopy("./src/admin/")
 
