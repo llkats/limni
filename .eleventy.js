651a73b82f45cf9b7bda0755715875d31228330f
Lydia Kats <llkats@gmail.com>
2023-01-07 12:57:21 -0600

    some nice looking post dates

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,15 @@
+const dayjs = require('dayjs')
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addLayoutAlias('post', 'post.njk')
   eleventyConfig.addLayoutAlias('blog', 'blog.njk')
 
   eleventyConfig.addPassthroughCopy("./src/index.css")
   eleventyConfig.addPassthroughCopy("./src/admin/")
+
+  eleventyConfig.addFilter("formatPostDate", (date) =>
+    dayjs(date).format('MMMM D, YYYY — h:mm a')
+  );
 
   return {
     passthroughFileCopy: true,
